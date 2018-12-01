9213054ea97deafa57a8826d5c74209277fe201e
Kasia <k.l.konstanty@gmail.com>
2018-12-01 15:57:04 +0100

    kk

--- hello2.java
+++ hello2.java
@@ -4,5 +4,6 @@ public class Hello2 {
         System.out.println("Konrad Stankiewicz");
         System.out.println("Krzysztof Magdziak");
         System.out.println("Katarzyna Chudobska");
+        System.out.println("Katarzyna Konstanty");
     }
 }
