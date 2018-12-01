5cb065d10d8cfd53830c9e13c255512a4acec009
Revolte <magdziak90@gmail.com>
2018-12-01 15:53:28 +0100

    Edit

--- hello2.java
+++ hello2.java
@@ -2,5 +2,6 @@ public class Hello2 {
     public static void main(String[] args) {
         System.out.println("Marcin Mazur");
         System.out.println("Konrad Stankiewicz");
+        System.out.println("Krzysztof Magdziak");
     }
 }
